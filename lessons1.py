8d378c57f6e0a4608372e4ba792f53bad2197a42
ostenbaken87 <ostenbaken87@mail.ru>
2023-07-31 10:57:30 +0800

    first lesson for python

---
+++ lessons1.py
@@ -0,0 +1,24 @@
+print('Hello world')
+x = 1
+print(x)
+print(type(x))
+x = float(x)
+print(x)
+x = 3
+y = 4
+print(x + y)
+# Площадь треугольника
+import math
+
+a = 15
+b = 11
+c = 9
+p = (a + b + c) / 2
+s = math.sqrt(p * (p - a) * (p - b) * (p - c))
+print(s)
+
+p = 3.14
+r = 12
+
+s = p * (r ** 2)
+print(s)
