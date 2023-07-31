5ee7b939a5fc46c26449ca86d89fda281d628480
ostenbaken87 <ostenbaken87@mail.ru>
2023-07-31 11:52:57 +0800

    String lesson for python

---
+++ lesson3.py
@@ -0,0 +1,29 @@
+x = 'Nik'
+print(x)
+
+y = "Some text"
+print(y)
+
+z = "Some 'long' text"
+print(z)
+
+q = 'Some \'long\', and \'awesome\' text'
+print(q)
+
+c = 'C:\\Users\\dell\\Desktop\\Py_lesson'
+print(c)
+
+t = r'C:\Users\dell\Desktop\Py_lesson'
+print(t)
+
+l = 'some long text \nand some long text \nand some long text'
+print(l)
+
+text = str('hello')
+print(text)
+print(text[0])
+print(text[0]+text[1])
+print(text[-1]+text[1])
+print(text[2:])
+print(text[::1])
+print(text[::2])
---
+++ lessons2.py
@@ -0,0 +1,5 @@
+print(1 < 2 and 2 < 3)  # True
+
+print(1 > 2 or 2 < 3)  # True
+
+print(1 > 2 or 2 > 3)  # False
